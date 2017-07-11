6a10fae704379fb48c671869385d78eae7e17bdf
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-12 06:40:08 +0800

    DirEntry

--- jvmgo/ch02/classpath/entry.go
+++ jvmgo/ch02/classpath/entry.go
@@ -3,6 +3,8 @@ package classpath
 import (
 	"os"
 	"strings"
+	"path/filepath"
+	"io/ioutil"
 )
 
 const pathlistSeqarator = string(os.PathListSeparator)
@@ -23,4 +25,27 @@ func newEntry(path string) Entry{
 		return newZipEntry(path)
 	}
 	return newDirEntry(path)
+}
+
+type DirEntry struct{
+	absDir string
+}
+
+func newDirEntry(path string) *DirEntry{
+	absDir, error := filepath.Abs(path)
+	if error != nil{
+		//终止函数执行
+		panic(error)
+	}
+	return &DirEntry{absDir}
+}
+
+func (self *DirEntry) readClass(className string) ([]byte, Entry, error)  {
+	fileName := filepath.Join(self.absDir, className)
+	data, error := ioutil.ReadFile(fileName)
+	return data, self, error
+}
+
+func (self *DirEntry) String() string  {
+	return self.absDir
 }
