3776bdbc7b4432848743a540cf5fb7746664ffed
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-12 06:33:59 +0800

    搜索类接口

---
+++ jvmgo/ch02/classpath/entry.go
@@ -0,0 +1,26 @@
+package classpath
+
+import (
+	"os"
+	"strings"
+)
+
+const pathlistSeqarator = string(os.PathListSeparator)
+
+type Entry interface {
+	readClass(className string) ([]byte, Entry, error)
+	String() string
+}
+
+func newEntry(path string) Entry{
+	if strings.Contains(path, pathlistSeqarator) {
+		return newCompositeEntry(path)
+	}
+	if strings.HasSuffix(path, "*") {
+		return newWildcardEntry(path)
+	}
+	if strings.HasSuffix(path, "jar") || strings.HasSuffix(path, ".JAR") || strings.HasSuffix(path, ".zip") || strings.HasSuffix(path, ".ZIP")) {
+		return newZipEntry(path)
+	}
+	return newDirEntry(path)
+}
