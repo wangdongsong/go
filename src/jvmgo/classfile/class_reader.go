57d55fdf7b9cbef1da6aa0ac1c70c8d503c95cce
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-20 06:48:46 +0800

    Uint16读取u2类型数据

--- src/jvmgo/classfile/class_reader.go
+++ src/jvmgo/classfile/class_reader.go
@@ -1,5 +1,7 @@
 package classfile
 
+import "encoding/binary"
+
 type ClassReader struct {
 	data []byte
 }
@@ -8,5 +10,12 @@ type ClassReader struct {
 func (self *ClassReader) readUint8() uint8 {
 	val := self.data[0]
 	self.data = self.data[1:]
+	return val
+}
+
+//使用Uint16读取u2类型数据
+func (self *ClassReader) readUint16() uint16 {
+	val := binary.BigEndian.Uint16(self.data)
+	self.data = self.data[2:]
 	return val
 }
