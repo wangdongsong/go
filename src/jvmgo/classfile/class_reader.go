f8dd4fd2bac6a217a7a2f5434ceee8807a5df065
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-20 06:54:42 +0800

    Uint16读取u4、u8、uint16表类型数据

--- src/jvmgo/classfile/class_reader.go
+++ src/jvmgo/classfile/class_reader.go
@@ -19,3 +19,34 @@ func (self *ClassReader) readUint16() uint16 {
 	self.data = self.data[2:]
 	return val
 }
+
+//读取u4类型数据
+func (self *ClassReader) readUint32() uint32  {
+	val := binary.BigEndian.Uint32(self.data)
+	self.data = self.data[4:]
+	return val;
+}
+
+//读取uint64(Java虚拟机规范并没有定义u8)类型数据
+func (self *ClassReader) readUint64() uint64  {
+	val := binary.BigEndian.Uint64(self.data)
+	self.data = self.data[8:0]
+	return val
+}
+
+//读取uint16表，表的大小由开头的uint16数据指出
+func (self *ClassReader) readUint16s()  []uint16 {
+	n := self.readUint16()
+	s := make([]uint16, n)
+	for i := range s{
+		s[i] = self.readUint16()
+	}
+	return s
+}
+
+//读取指定数量的字节
+func (self *ClassReader) readBytes(n uint32) []byte  {
+	bytes := self.data[n:]
+	self.data = self.data[n:]
+	return bytes
+}
