5c07990d9e8f68e74803e30b22121473d2908f6f
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-21 22:52:16 +0800

    read method

--- src/jvmgo/classfile/class_file.go
+++ src/jvmgo/classfile/class_file.go
@@ -34,6 +34,18 @@ func Parse(classData []byte) (cf *ClassFile, err error){
 	return
 }
 
+func (self *ClassFile) read(reader *ClassReader) {
+	self.readAndCheckmagic(reader)
+	self.readAndCheckVersion(reader)
+	self.constantPool = readConstantPool(reader)
+	self.accessFlags = reader.readUint16()
+	self.thisClass = reader.readUint16()
+	self.superClass = reader.readUint16()
+	self.interfaces = reader.readUint16s()
+	self.fields = readMembers(reader, self.constantPool)
+	self.methods = readMembers(reader, self.constantPool)
+	self.attributes = readAttributes(reader, self.constantPool)
+}
 
 
 
