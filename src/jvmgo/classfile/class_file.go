ff71902d1b174c1be3e48f1c4a9b7b5fda227eb8
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-24 06:33:03 +0800

    版本号

--- src/jvmgo/classfile/class_file.go
+++ src/jvmgo/classfile/class_file.go
@@ -79,3 +79,18 @@ func (self *ClassFile) readAndCheckMagic(reader *ClassReader){
 		panic("java.lang.ClassFormatError:magic")
 	}
 }
+
+//版本号
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader){
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46,47,48,49,50,51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+	panic("java.lang.UnsupportedClassVersionError!")
+}
