fde485499f36151f23dfa7b926cb8049a77e14e4
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-25 06:51:25 +0800

    常量池

--- src/jvmgo/classfile/constant_pool.go
+++ src/jvmgo/classfile/constant_pool.go
@@ -15,4 +15,31 @@ func readConstantPool(reader *ClassReader) ConstantPool {
 	}
 }
 
+func (self ConstantPool) getConstantPool(index uint16) ConstantPool{
+	if cpInfo := self[index]; cpInfo != nil {
+		return cpInfo
+	}
+
+	panic("Invalid constant pool index!")
+
+}
+
+func(self ConstantPool) getNameAndType(index uint16) (string, string){
+	ntInfo := self.getConstantPool(index).(*ConstantNameAndTypeInfo)
+	name := self.getUtf8(ntInfo.nameIndex)
+	_type := self.getUtf8(ntInfo.descriptorIndex)
+	return name, _type
+}
+
+func (self ConstantPool) getClasName(index uint16) string{
+	classInfo := self.getConstantPool(index).(*ConstantClassInfo)
+	return self.getUtf8(classInfo.nameIndex)
+}
+
+func (self ConstantPool) getUtf8(index uint16) string{
+	utf8Info := self.getConstantPool(index).(*ConstantUtf8Info)
+	return utf8Info.str
+}
+
+
 
