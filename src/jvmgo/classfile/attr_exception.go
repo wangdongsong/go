45a99267e24c2b4d9aa8427dd79a72e3e1dcc195
wangdongsong1229 <wangdongsong1229@163.com>
2017-07-31 06:27:23 +0800

    exception

---
+++ src/jvmgo/classfile/attr_exception.go
@@ -0,0 +1,14 @@
+package classfile
+
+//exceptions是变长属性，记录方法抛出的方法表
+type ExceptionsAttribute struct {
+	exceptionIndexTable []uint16
+}
+
+func (self *ExceptionsAttribute) readInfo(reader *ClassReader){
+	self.exceptionIndexTable = reader.readUint16s()
+}
+
+func (self *ExceptionsAttribute) ExceptionIndexTable() []uint16{
+	return self.exceptionIndexTable
+}
