9da15059c2b6f314dd509e2fbe8f8ca7e32c57e8
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-02 22:32:55 +0800

    对象

---
+++ src/jvmgo/rtda/object.go
@@ -0,0 +1,23 @@
+package rtda
+
+/*
+ Java	go
+ byte 	int8
+ short	int16
+ int 	int32
+ long	int64
+ char	uint16
+ float 	float32
+ double	float64
+ boolean	bool
+ 类类型		*Object
+ 接口类型	*Object
+ 数组类型	*Object
+ null		nil
+ */
+
+type Object struct {
+
+}
+
+
