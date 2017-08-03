a4394629d3d5a40330e2c17963f7cd9bb1b94663
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-04 06:26:02 +0800

    常量指令

---
+++ src/jvmgo/instruction/constants/const.go
@@ -0,0 +1,79 @@
+package constants
+
+import (
+	"jvmgo/instruction"
+	"jvmgo/rtda"
+)
+
+type ACONST_NULL struct {
+	instruction.NoOperandsInstruction
+}
+
+type DCONST_0 struct {
+	instruction.NoOperandsInstruction
+}
+
+type DCONST_1 struct {
+	instruction.NoOperandsInstruction
+}
+
+type FCONST_0 struct {
+	instruction.NoOperandsInstruction
+}
+
+type FCONST_1 struct {
+	instruction.NoOperandsInstruction
+}
+
+type FCONST_2 struct {
+	instruction.NoOperandsInstruction
+}
+
+type ICONST_M1 struct {
+	instruction.NoOperandsInstruction
+}
+
+type ICONST_0 struct {
+	instruction.NoOperandsInstruction
+}
+
+type ICONST_1 struct {
+	instruction.NoOperandsInstruction
+}
+
+type ICONST_2 struct {
+	instruction.NoOperandsInstruction
+}
+
+type ICONST_3 struct {
+	instruction.NoOperandsInstruction
+}
+
+type ICONST_4 struct {
+	instruction.NoOperandsInstruction
+}
+
+type ICONST_5 struct {
+	instruction.NoOperandsInstruction
+}
+
+type LCONST_0 struct {
+	instruction.NoOperandsInstruction
+}
+
+type LCONST_1 struct {
+	instruction.NoOperandsInstruction
+}
+
+func (self *ACONST_NULL) Execute(frame *rtda.Frame) {
+	frame.OperandStack().PushNull()
+}
+
+func (self *DCONST_0) Execute(frame rtda.Frame) {
+	frame.OperandStack().PushDouble(0.0)
+}
+
+//iconst_m1指令把int型-1推入操作数栈顶
+func (self *ICONST_M1) Execute(frame rtda.Frame)  {
+	frame.OperandStack().PushInt(-1)
+}
