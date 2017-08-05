f46c3711306d4abe8d7a7cc36d6c218cf50147e0
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-05 08:00:11 +0800

    dup指令

---
+++ src/jvmgo/instruction/stack/dup.go
@@ -0,0 +1,93 @@
+package stack
+
+import (
+	"jvmgo/instruction"
+	"jvmgo/rtda"
+)
+
+type DUP struct {
+	instruction.NoOperandsInstruction
+}
+
+type DUP_X1 struct {
+	instruction.NoOperandsInstruction
+}
+
+type DUP_X2 struct {
+	instruction.NoOperandsInstruction
+}
+
+type DUP2 struct {
+	instruction.NoOperandsInstruction
+}
+
+type DUP2_X1 struct {
+	instruction.NoOperandsInstruction
+}
+
+type DUP2_X2 struct {
+	instruction.NoOperandsInstruction
+}
+
+func (self *DUP) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	slot := stack.PopSlot()
+	stack.PushSlot(slot)
+	stack.PushSlot(slot)
+}
+
+func (self *DUP_X1) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val1 := stack.PopSlot()
+	val2 := stack.PopSlot()
+	stack.PushSlot(val1)
+	stack.PushSlot(val2)
+	stack.PushSlot(val1)
+}
+
+func (self *DUP_X2) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val1 := stack.PopSlot()
+	val2 := stack.PopSlot()
+	val3 := stack.PopSlot()
+	stack.PushSlot(val1)
+	stack.PushSlot(val3)
+	stack.PushSlot(val2)
+	stack.PushSlot(val1)
+}
+
+func (self *DUP2) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val1 := stack.PopSlot()
+	val2 := stack.PopSlot()
+	stack.PushSlot(val2)
+	stack.PushSlot(val1)
+	stack.PushSlot(val2)
+	stack.PushSlot(val1)
+}
+
+func (self *DUP2_X1) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val1 := stack.PopSlot()
+	val2 := stack.PopSlot()
+	val3 := stack.PopSlot()
+	stack.PushSlot(val2)
+	stack.PushSlot(val1)
+	stack.PushSlot(val3)
+	stack.PushSlot(val2)
+	stack.PushSlot(val1)
+}
+
+func (self *DUP2_X2) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	val1 := stack.PopSlot()
+	val2 := stack.PopSlot()
+	val3 := stack.PopSlot()
+	val4 := stack.PopSlot()
+	stack.PushSlot(val2)
+	stack.PushSlot(val1)
+	stack.PushSlot(val4)
+	stack.PushSlot(val3)
+	stack.PushSlot(val2)
+	stack.PushSlot(val1)
+}
