56d09cb667d30bc55ef56c27322dab2dd445b7b3
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-06 11:40:08 +0800

    l2x

---
+++ src/jvmgo/instruction/conversions/l2x.go
@@ -0,0 +1,36 @@
+package conversions
+
+import (
+	"jvmgo/rtda"
+	"jvmgo/instruction"
+)
+
+// Convert long to double
+type L2D struct{ instruction.NoOperandsInstruction }
+
+func (self *L2D) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	l := stack.PopLong()
+	d := float64(l)
+	stack.PushDouble(d)
+}
+
+// Convert long to float
+type L2F struct{ instruction.NoOperandsInstruction }
+
+func (self *L2F) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	l := stack.PopLong()
+	f := float32(l)
+	stack.PushFloat(f)
+}
+
+// Convert long to int
+type L2I struct{ instruction.NoOperandsInstruction }
+
+func (self *L2I) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	l := stack.PopLong()
+	i := int32(l)
+	stack.PushInt(i)
+}
