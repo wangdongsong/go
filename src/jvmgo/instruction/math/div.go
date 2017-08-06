6059eed17e42caded050b24b66fd8cd1868afe35
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-06 11:46:45 +0800

    div

---
+++ src/jvmgo/instruction/math/div.go
@@ -0,0 +1,60 @@
+package math
+
+import (
+	"jvmgo/rtda"
+	"jvmgo/instruction"
+)
+
+// Divide double
+type DDIV struct{ instruction.NoOperandsInstruction }
+
+func (self *DDIV) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopDouble()
+	v1 := stack.PopDouble()
+	result := v1 / v2
+	stack.PushDouble(result)
+}
+
+// Divide float
+type FDIV struct{ instruction.NoOperandsInstruction }
+
+func (self *FDIV) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopFloat()
+	v1 := stack.PopFloat()
+	result := v1 / v2
+	stack.PushFloat(result)
+}
+
+// Divide int
+type IDIV struct{ instruction.NoOperandsInstruction }
+
+func (self *IDIV) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+
+	if v2 == 0 {
+		frame.Thread().ThrowDivByZero()
+	} else {
+		result := v1 / v2
+		stack.PushInt(result)
+	}
+}
+
+// Divide long
+type LDIV struct{ instruction.NoOperandsInstruction }
+
+func (self *LDIV) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+
+	if v2 == 0 {
+		frame.Thread().ThrowDivByZero()
+	} else {
+		result := v1 / v2
+		stack.PushLong(result)
+	}
+}
