d9aea0cfda1beefec90d1596c5a8bc129a5c441f
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-06 11:22:21 +0800

    iinc指令

---
+++ src/jvmgo/instruction/math/iinc.go
@@ -0,0 +1,26 @@
+package math
+
+import (
+	"jvmgo/instruction"
+	"jvmgo/rtda"
+)
+
+//iinc指令
+//iinc指令给局部变量表的int变量增加常量值，局部变量表索引和常量值都由指令的操作数提供
+
+type IINC struct {
+	Index uint
+	Const int32
+}
+
+func (self *IINC) FetchOperands(reader *instruction.BytecodeReader) {
+	self.Index = uint(reader.ReadUint8())
+	self.Const = int32(reader.ReadInt8())
+}
+
+func (self *IINC) Execute(frame *rtda.Frame) {
+	localVars := frame.LocalVars()
+	val := localVars.GetInt(self.Index)
+	val += self.Const
+	localVars.SetInt(self.Index, val)
+}
