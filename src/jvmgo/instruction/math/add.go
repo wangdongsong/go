052152a68e5fb44ebd413ca765078f1f87355631
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-06 11:46:37 +0800

    add

---
+++ src/jvmgo/instruction/math/add.go
@@ -0,0 +1,50 @@
+package math
+
+import (
+	"jvmgo/rtda"
+	"jvmgo/instruction"
+)
+
+// Add double
+type DADD struct{ instruction.NoOperandsInstruction }
+
+func (self *DADD) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v1 := stack.PopDouble()
+	v2 := stack.PopDouble()
+	result := v1 + v2
+	stack.PushDouble(result)
+}
+
+// Add float
+type FADD struct{ instruction.NoOperandsInstruction }
+
+func (self *FADD) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopFloat()
+	v1 := stack.PopFloat()
+	result := v1 + v2
+	stack.PushFloat(result)
+}
+
+// Add int
+type IADD struct{ instruction.NoOperandsInstruction }
+
+func (self *IADD) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	result := v1 + v2
+	stack.PushInt(result)
+}
+
+// Add long
+type LADD struct{ instruction.NoOperandsInstruction }
+
+func (self *LADD) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+	result := v1 + v2
+	stack.PushLong(result)
+}
