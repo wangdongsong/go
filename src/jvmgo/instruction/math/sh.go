9063f7b9e438d0fc61c8a870f8dafdd6b42812a2
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-06 11:11:39 +0800

    位移指令

---
+++ src/jvmgo/instruction/math/sh.go
@@ -0,0 +1,99 @@
+package math
+
+import (
+	"jvmgo/instruction"
+	"jvmgo/rtda"
+)
+
+//位移指令
+
+//int 左位移
+type ISHL struct {
+	instruction.NoOperandsInstruction
+}
+
+//int 算术右位移
+type ISHR struct {
+	instruction.NoOperandsInstruction
+}
+
+//int逻辑右位移
+type IUSHR struct {
+	instruction.NoOperandsInstruction
+}
+
+//long左位移
+type LSHL struct {
+	instruction.NoOperandsInstruction
+}
+
+//long算术右位移
+type LSHR struct {
+	instruction.NoOperandsInstruction
+}
+
+//long逻辑右位移
+type LUSHR struct {
+	instruction.NoOperandsInstruction
+}
+
+//左移指令
+func (self *ISHL) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	s := uint32(v2) & 0x1f
+	result := v1 << s
+	stack.PushInt(result)
+}
+
+//左移
+func (self *ISHR) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	s := uint32(v2) & 0x1f
+	result := v1 >> s
+	stack.PushInt(result)
+}
+
+//逻辑右移
+func (self *IUSHR) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	s := uint32(v2) & 0x1f
+	result := int32(uint32(v1) >> s)
+	stack.PushInt(result)
+}
+
+func (self *LSHL) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopLong()
+	s := uint32(v2) & 0x3f
+	result := v1 << s
+	stack.PushLong(result)
+}
+
+
+//算术右移
+func (self *LSHR) Execute(frame *rtda.Frame){
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopLong()
+	s := uint32(v2) & 0x3f
+	result := v1 >> s
+	stack.PushLong(result)
+
+}
+
+
+func (self *LUSHR) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopLong()
+	s := uint32(v2) & 0x3f
+	result := int64(uint64(v1) >> s)
+	stack.PushLong(result)
+}
