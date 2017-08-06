0b45944a148b76b8dd3176afc6e0e00acbd43731
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-06 11:47:07 +0800

    sub

---
+++ src/jvmgo/instruction/math/sub.go
@@ -0,0 +1,50 @@
+package math
+
+import (
+	"jvmgo/rtda"
+	"jvmgo/instruction"
+)
+
+// Subtract double
+type DSUB struct{ instruction.NoOperandsInstruction }
+
+func (self *DSUB) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopDouble()
+	v1 := stack.PopDouble()
+	result := v1 - v2
+	stack.PushDouble(result)
+}
+
+// Subtract float
+type FSUB struct{ instruction.NoOperandsInstruction }
+
+func (self *FSUB) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopFloat()
+	v1 := stack.PopFloat()
+	result := v1 - v2
+	stack.PushFloat(result)
+}
+
+// Subtract int
+type ISUB struct{ instruction.NoOperandsInstruction }
+
+func (self *ISUB) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	result := v1 - v2
+	stack.PushInt(result)
+}
+
+// Subtract long
+type LSUB struct{ instruction.NoOperandsInstruction }
+
+func (self *LSUB) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+	result := v1 - v2
+	stack.PushLong(result)
+}
