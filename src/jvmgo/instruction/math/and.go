b2d0a3d645dcb9d5f24cd1dfa7fbd5166c0e6e5c
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-06 11:18:46 +0800

    按位与

---
+++ src/jvmgo/instruction/math/and.go
@@ -0,0 +1,32 @@
+package math
+
+import (
+	"jvmgo/instruction"
+	"jvmgo/rtda"
+)
+
+//布尔运算指令
+
+type IAND struct {
+	instruction.NoOperandsInstruction
+}
+
+type LAND struct {
+	instruction.NoOperandsInstruction
+}
+
+func (self *IAND) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	result := v1 & v2;
+	stack.PushInt(result)
+}
+
+func (self *LAND) Execute(frmae *rtda.Frame) {
+	stack := frmae.OperandStack();
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+	result := v1 & v2
+	stack.PushLong(result)
+}
