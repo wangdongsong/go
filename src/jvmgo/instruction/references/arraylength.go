bbc0529585bdf8a8cac3a0581836324b1cedee0f
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-11 22:48:32 +0800

    array

---
+++ src/jvmgo/instruction/references/arraylength.go
@@ -0,0 +1,23 @@
+package references
+
+import (
+	"jvmgo/instruction"
+	"jvmgo/rtda"
+	"jvmgo/rtda/heap"
+)
+
+// Get length of array
+type ARRAY_LENGTH struct{ instruction.NoOperandsInstruction }
+
+func (self *ARRAY_LENGTH) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	arrRef := stack.PopRef()
+
+	if arrRef == nil {
+		frame.Thread().ThrowNPE()
+		return
+	}
+
+	arrLen := heap.ArrayLength(arrRef)
+	stack.PushInt(arrLen)
+}
