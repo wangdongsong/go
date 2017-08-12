e4f06b55c7de01bb57e0b425c92dd6bf72abb1ec
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-12 09:52:45 +0800

    athrow

---
+++ src/jvmgo/instruction/references/athrow.go
@@ -0,0 +1,38 @@
+package references
+
+import (
+	"jvmgo/instruction"
+	"jvmgo/rtda"
+)
+// Throw exception or error
+type ATHROW struct{ instruction.NoOperandsInstruction }
+
+func (self *ATHROW) Execute(frame *rtda.Frame) {
+	ex := frame.OperandStack().PopRef()
+	if ex == nil {
+		frame.Thread().ThrowNPE()
+		return
+	}
+
+	thread := frame.Thread()
+	for {
+		frame := thread.CurrentFrame()
+		pc := frame.NextPC() - 1
+
+		handler := frame.Method().FindExceptionHandler(ex.Class(), pc)
+		if handler != nil {
+			stack := frame.OperandStack()
+			stack.Clear()
+			stack.PushRef(ex)
+			frame.SetNextPC(handler.HandlerPc())
+			return
+		}
+
+		thread.PopFrame()
+		if thread.IsStackEmpty() {
+			break
+		}
+	}
+
+	thread.HandleUncaughtException(ex)
+}
