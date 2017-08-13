fa56b06bb55857b89dbe573ab6164c60e770fb72
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-14 06:32:18 +0800

    jvm

---
+++ src/jvmgo/jvm.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	"runtime/pprof"
+
+	"jvmgo/classpath"
+	"jvmgo/cmdline"
+	"jvmgo/jutil"
+	"jvmgo/interpreter"
+	"jvmgo/options"
+	"jvmgo/rtda"
+	"jvmgo/rtda/heap"
+	_"jvmgo/native"
+)
+
+func startJVM(cmd *cmdline.Command) {
+	Xcpuprofile := cmd.Options().Xcpuprofile
+	if Xcpuprofile != "" {
+		f, err := os.Create(Xcpuprofile)
+		if err != nil {
+			panic(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	options.InitOptions(cmd.Options())
+
+	cp := classpath.Parse(cmd.Options().Classpath())
+	heap.InitBootLoader(cp)
+
+	mainClassName := jutil.ReplaceAll(cmd.Class(), ".", "/")
+	mainThread := createMainThread(mainClassName, cmd.Args())
+	interpreter.Loop(mainThread)
+	interpreter.KeepAlive()
+}
+
+func createMainThread(className string, args []string) *rtda.Thread {
+	mainThread := rtda.NewThread(nil)
+	bootMethod := heap.BootstrapMethod()
+	bootArgs := []interface{}{className, args}
+	mainThread.InvokeMethodWithShim(bootMethod, bootArgs)
+	return mainThread
+}
