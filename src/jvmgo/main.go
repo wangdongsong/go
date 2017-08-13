ab43519704807f6f535edaae15995b4ddbc15593
wangdongsong1229 <wangdongsong1229@163.com>
2017-08-14 06:32:42 +0800

    main

---
+++ src/jvmgo/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"jvmgo/cmdline"
+)
+
+func main() {
+	cmd, err := cmdline.ParseCommand(os.Args)
+	if err != nil {
+		cmdline.PrintUsage()
+	} else {
+		startJVM(cmd)
+	}
+}
