0b847820ccb6b0d22328faea5ade85c4ec5ae971
wangdongsong <wangdongsong1229@163.com>
2017-11-10 06:23:22 +0800

    updated

--- src/mydocker/docker/main.go
+++ src/mydocker/docker/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/urfave/cli"
+	log "github.com/sirupsen/logrus"
+	"mydocker/docker/container"
+	"fmt"
 )
 
 const usage = "mydocker is a simple container runtime implementation. The Purpose" +
@@ -14,5 +17,45 @@ func main()  {
 	app.Name = "mydocker"
 	app.Usage = usage
 
-	app.Commands = []cli.Command{initCommand, runCommand}
+	app.Commands = []cli.Command{initCommand, runCommand, }
+}
+
+var runCommand = cli.Command{
+	Name : "run",
+	Usage: "Create a container with namespace and cgroups limit mydocker run -ti[comman]",
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name: "ti",
+			Usage: "enable tty",
+		},
+	},
+
+	Action: func(context cli.Context) error {
+		if len(context.Args()) < 1 {
+			return fmt.Errorf("Missing container command")
+		}
+
+		cmd := context.Args().Get(0)
+		tty := context.Bool("ti")
+		Run(tty, cmd)
+		return nil
+	},
+}
+
+
+var initCommand = cli.Command{
+	Name: "init",
+	Usage: "Init container process run user's process in container. Do not call it outside",
+
+	Action: func(context cli.Context) error {
+		log.Info("Init come on")
+		cmd := context.Args().Get(0)
+		log.Infof("command %s", cmd)
+		err := container.RunContainerInitProcess(cmd, nil)
+		return err
+	},
+}
+
+func Run(tty bool, command string){
+
 }
