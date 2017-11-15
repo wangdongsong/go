7d6aeef86c4a1ee11e26bbde9cf22dc4b47117e9
wangdongsong <wangdongsong1229@163.com>
2017-11-16 06:54:08 +0800

    file io

--- src/io/file.go
+++ src/io/file.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+//IO
 func main() {
-	fmt.Print("Hello")
+
+	fmt.Println(os.Args[1])
+
+	fileInfo, err := os.Stat(os.Args[1])
+
+	fmt.Println(fileInfo.IsDir())
+	fmt.Println(err)
+
+	if err != nil && fileInfo.IsDir() {
+		fmt.Println(fileInfo.IsDir())
+	}
+
+
+	//获取当前执行程序的路径
+	//file, _ := exec.LookPath(os.Args[0])
+
 }
