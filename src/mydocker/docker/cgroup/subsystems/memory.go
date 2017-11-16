223c85c901c42c53b17e97e655f5d8bcfadfa3a1
wangdongsong <wangdongsong1229@163.com>
2017-11-17 06:28:15 +0800

    memory limit

--- src/mydocker/docker/cgroup/subsystems/memory.go
+++ src/mydocker/docker/cgroup/subsystems/memory.go
@@ -1,18 +1,18 @@
 package subsystems
 
 import (
+	"fmt"
 	"io/ioutil"
 	"path"
-	"fmt"
 )
 
-type MemorySubSystem struct {}
+type MemorySubSystem struct{}
 
-func(s *MemorySubSystem) Set(cgroupPath string, res *ResourceConfig) error {
+func (s *MemorySubSystem) Set(cgroupPath string, res *ResourceConfig) error {
 
 	if subsysCgroupPath, err := GetCgroupPath(s.Name(), cgroupPath, true); err == nil {
 
-		if err := ioutil.WriteFile(path.Join(subsysCgroupPath, "memory.limit_in_bytes"), []byte(res.MemoryLimit), 0644); err != nil{
+		if err := ioutil.WriteFile(path.Join(subsysCgroupPath, "memory.limit_in_bytes"), []byte(res.MemoryLimit), 0644); err != nil {
 			return fmt.Errorf("set cgroup memory fail %v", err)
 		}
 		return nil
@@ -20,7 +20,6 @@ func(s *MemorySubSystem) Set(cgroupPath string, res *ResourceConfig) error {
 		return err
 	}
 }
-
 
 func (s *MemorySubSystem) Name() string {
 	return "memory"
