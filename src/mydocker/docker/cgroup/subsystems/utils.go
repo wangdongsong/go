d28507f5f25095ceac373c1f6e2f014f8af2be3d
wangdongsong <wangdongsong1229@163.com>
2017-11-17 06:28:48 +0800

    update

--- src/mydocker/docker/cgroup/subsystems/utils.go
+++ src/mydocker/docker/cgroup/subsystems/utils.go
@@ -1,7 +1,57 @@
 package subsystems
 
-func GetCgroupPath(subsystem string, cgroupPath string, autoCreate bool) (string, error) {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
 
-	return "s", nil;
+func FindCgroupMountpoint(subsystem string) string {
+
+	f, err := os.Open("/proc/self/mountinfo")
+
+	if err != nil {
+		return ""
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		txt := scanner.Text()
+
+		fields := strings.Split(txt, " ")
+		for _, opt := range strings.Split(fields[len(fields)-1], ",") {
+			if opt == subsystem {
+				return fields[4]
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return ""
+	}
+
+	return ""
+
 }
 
+func GetCgroupPath(subsystem string, cgroupPath string, autoCreate bool) (string, error) {
+
+	cgroupRoot := FindCgroupMountpoint(subsystem)
+	if _, err := os.Stat(path.Join(cgroupRoot, cgroupPath)); err == nil || (autoCreate && os.IsNotExist(err)) {
+		if os.IsNotExist(err) {
+			if err := os.Mkdir(path.Join(cgroupRoot, cgroupPath), 0755); err == nil {
+				//nothing
+			} else {
+				return "", fmt.Errorf("error create cgroup %v", err)
+			}
+		}
+		return path.Join(cgroupRoot, cgroupPath), nil
+	} else {
+		return "", fmt.Errorf("cgroup path error %v", err)
+	}
+}
