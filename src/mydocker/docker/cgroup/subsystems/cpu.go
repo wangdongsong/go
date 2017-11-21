fd26ecdadd11cb1d64870045fd113243cf648b16
wangdongsong <wangdongsong1229@163.com>
2017-11-21 08:32:05 +0800

    cpu

---
+++ src/mydocker/docker/cgroup/subsystems/cpu.go
@@ -0,0 +1,26 @@
+package subsystems
+
+import (
+	"io/ioutil"
+	"path"
+	"fmt"
+)
+
+type CpuSubSystem struct{}
+
+func (s *CpuSubSystem) Set(cgroupPath string, res *ResourceConfig) error {
+	if subsysCgroupPath, err := GetCgroupPath(s.Name(), cgroupPath, true); err == nil {
+		if res.CpuShare != "" {
+			if err := ioutil.WriteFile(path.Join(subsysCgroupPath, "cpu.shares"), []byte(res.CpuShare), 0644); err != nil {
+				return fmt.Errorf("set cgroup cpu share fail %v", err)
+			}
+		}
+		return nil
+	} else {
+		return err
+	}
+}
+
+func (s *CpuSubSystem) Name() string{
+	return "cpu"
+}
